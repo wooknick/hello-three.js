ed87dbb7b0a584451324d74a64dbbb1e824c3b3f
wooknick <wooknick@naver.com>
2019-11-19 19:42:47 +0900

    Drawing lines

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,46 @@
 import * as THREE from "three";
 
+// Scene 설정
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
 
+// Renderer 설정
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "white" });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Camera 설정
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  500
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-camera.position.z = 5;
+// Material 설정
+const material = new THREE.LineBasicMaterial({ color: "green" });
 
-const animate = () => {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-};
-animate();
+// Geometry 설정
+const geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3(-10, 0, 0)); // 꼭지점들
+geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+
+// Object 생성
+const line = new THREE.Line(geometry, material);
+
+// Scene 내에 Object 추가
+scene.add(line);
+
+// Scene Render
+renderer.render(scene, camera);
+
+// Animation
+// const animate = () => {
+//   requestAnimationFrame(animate);
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
+//   renderer.render(scene, camera);
+// };
+// animate();
